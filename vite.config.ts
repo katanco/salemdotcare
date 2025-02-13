7f1c035ad124c5348255c137bd0f9200d99ff62b
Jonathan Schmitt <jonathan.schmitt@theorangeforce.us>
2025-02-12 17:41:52 -0700

    deploy test cont

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/salemdotcare",
-  build: {
-    outDir: "./build",
-    emptyOutDir: true, // also necessary
-  },
+  base: "/salemdotcare"
 });
